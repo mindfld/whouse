00e9f724fef40c3fcc0904cac82db98ae3641fce
Sergiy Lesko <mindfld@gmail.com>
2014-06-03 18:17:02 +0300

    Database changed

--- database.sql
+++ database.sql
@@ -1,113 +1,55 @@
-CREATE DATABASE  IF NOT EXISTS `whouse` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `whouse`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
---
--- Host: localhost    Database: whouse
--- ------------------------------------------------------
--- Server version	5.7.3-m13
+DROP DATABASE whouse; 
+CREATE DATABASE whouse  DEFAULT CHARACTER SET koi8u  DEFAULT COLLATE koi8u_general_ci;
+USE whouse;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS commodity;
+DROP TABLE IF EXISTS storage;
 
---
--- Table structure for table `commodity`
---
+CREATE TABLE commodity (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  name varchar(255) COLLATE koi8u_general_ci  NOT NULL,
+  description varchar(255) COLLATE koi8u_general_ci  DEFAULT NULL,
+  measurementUnit varchar(5) COLLATE koi8u_general_ci  DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+);
 
-DROP TABLE IF EXISTS `commodity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `commodity` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `measurementUnit` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `storage_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK4D84270D3F04DE12` (`storage_id`),
-  CONSTRAINT `FK4D84270D3F04DE12` FOREIGN KEY (`storage_id`) REFERENCES `storage` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE storage (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  commodity_id bigint(20) DEFAULT NULL,
+  available_quantity int(11) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id),
+  CONSTRAINT FOREIGN KEY (commodity_id) REFERENCES commodity (id)
+);
 
---
--- Dumping data for table `commodity`
---
+CREATE TABLE roles (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  role_name bigint(20) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+);
 
-LOCK TABLES `commodity` WRITE;
-/*!40000 ALTER TABLE `commodity` DISABLE KEYS */;
-INSERT INTO `commodity` VALUES (1,'Перфоратор DWT BH-1500',0,'Перфоратор DWT BH-1500',NULL),(2,'Бур Hitachi 40mm',0,'Бур Hitachi 40mm',NULL),(3,'Дюбель универсальный 6*40',0,'Дюбель 6*40',NULL),(4,'Манометричний коллектор',0,'Манометричний коллектор',NULL),(5,'Трубогиб пружинный 1/4 СТ-102-04',0,'Трубогиб пружинный 1/4',NULL),(6,'Трубогиб пружинный 3/8 СТ-102-06',0,'Трубогиб пружинный 3/8',NULL),(7,'Труборез CT 127 (3-16 мм)',0,'Труборез 3-16 мм',NULL),(8,'Вальцовка СТ-806АМ дюймовая',0,'Вальцовка дюймовая',NULL),(9,'Манометрический коллектор 536G R-22,12,407+3шланга (90см.) в комплекте Харьков',0,'Манометрический коллектор 536G ',NULL),(10,'хладагентом R22.Прикольний',2,'хладагентом R22',NULL),(11,'хладагентом R32.Круче',2,'хладагентом R32',NULL),(12,'Ізолента чорна',4,'Ізолента чорна',NULL),(13,'Цвяхи стальні 100мм',1,'Цвяхи і 100мм',NULL);
-/*!40000 ALTER TABLE `commodity` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE users (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  login varchar(45) COLLATE koi8u_general_ci  DEFAULT NULL,
+  password varchar(45) COLLATE koi8u_general_ci  DEFAULT NULL,
+  fullname varchar(45) COLLATE koi8u_general_ci  DEFAULT NULL,
+  role_id bigint(20) NOT NULL ,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id), 
+  CONSTRAINT FOREIGN KEY (role_id) REFERENCES roles (id)
+);
 
---
--- Table structure for table `storage`
---
-
-DROP TABLE IF EXISTS `storage`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `storage` (
-  `QUANTITY` int(11) NOT NULL,
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `commodity_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `FKBA7BCE5B6C06C5F` (`commodity_id`),
-  CONSTRAINT `FKBA7BCE5B6C06C5F` FOREIGN KEY (`commodity_id`) REFERENCES `commodity` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `storage`
---
-
-LOCK TABLES `storage` WRITE;
-/*!40000 ALTER TABLE `storage` DISABLE KEYS */;
-INSERT INTO `storage` VALUES (4,1,1),(4,2,2),(5,3,3),(7,4,4),(4,5,5),(55,6,6),(6,7,7),(3,8,8),(57,9,9),(3,10,10),(2,11,11),(4,12,12),(6,13,13);
-/*!40000 ALTER TABLE `storage` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `login` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `role` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'order','OrderUser','order',2),(2,'mind','Sergiy Lesko','medusa',2);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-01-27 12:50:35
+CREATE TABLE flows (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  commodity_id bigint(20) NOT NULL,
+  user_id bigint(20) NOT NULL,
+  balance DECIMAL(6,2) NOT NULL,
+  comments varchar(180) COLLATE koi8u_general_ci ,  
+  flow_date TIMESTAMP NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id), 
+  CONSTRAINT FOREIGN KEY (commodity_id) REFERENCES commodity (id),
+  CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (id)
+);
--- database2.sql
+++ database2.sql
@@ -1,350 +1,52 @@
-CREATE DATABASE  IF NOT EXISTS `whouse` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `whouse`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
---
--- Host: 127.0.0.1    Database: whouse
--- ------------------------------------------------------
--- Server version	5.7.3-m13
+INSERT INTO commodity VALUES 
+(2,'Бур Hitachi 40mm','Бур Hitachi 40mm','шт'),
+(3,'Дюбель 6*40','Дюбель универсальный 6*40','кг'),
+(4,'Манометричний коллектор','Манометричний коллектор','шт'),
+(5,'Трубогиб пружинный 1/4','Трубогиб пружинный 1/4 СТ-102-04','шт'),
+(6,'Трубогиб пружинный 3/8','Трубогиб пружинный 3/8 СТ-102-06','шт'),
+(7,'Труборез 3-16 мм','Труборез CT 127 (3-16 мм)','шт'),
+(8,'Вальцовка дюймовая','Вальцовка СТ-806АМ дюймовая','шт'),
+(9,'Манометрический коллектор 536G ','Манометрический коллектор 536G R-22,12,407+3шланга (90см.) в комплекте Харьков','шт'),
+(10,'хладагентом R22','хладагентом R22.Прикольний','л'),
+(11,'хладагентом R32','хладагентом R32.Круче','шт'),
+(12,'Ізолента чорна','Ізолента чорна','м'),
+(13,'Цвяхи і 100мм','Цвяхи стальні 100мм','кг');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+INSERT INTO storage VALUES
+(1,2,12),
+(2,3,13),
+(3,4,5),
+(4,5,6),
+(5,6,44),
+(6,7,66),
+(7,8,16),
+(8,9,18),
+(9,10,44),
+(10,11,3),
+(11,12,11),
+(12,13,10);
 
---
--- Table structure for table `categories_commodity`
---
+INSERT INTO roles VALUES 
+(1,'Комірник'),
+(2,'Робочий'),
+(3,'Бухгалтер'),
+(4,'Директор'),
+(5,'Адміністратор');
 
-DROP TABLE IF EXISTS `categories_commodity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categories_commodity` (
-  `CATEGORIES_id` bigint(20) NOT NULL,
-  `commodityList_id` bigint(20) NOT NULL,
-  UNIQUE KEY `commodityList_id` (`commodityList_id`),
-  KEY `FK9A360AADAAB7D81` (`commodityList_id`),
-  CONSTRAINT `FK9A360AADAAB7D81` FOREIGN KEY (`commodityList_id`) REFERENCES `commodity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `categories_commodity`
---
 
-LOCK TABLES `categories_commodity` WRITE;
-/*!40000 ALTER TABLE `categories_commodity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories_commodity` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO users VALUES
+(1,'mind','medusa','Сергій Лесько',5)
+(2,'vitas','bvi','Віктор Бачило',4)
+(3,'buh','buh','Бухгалтер Зіна',3)
+(4,'rob','rob','Робочий Коля',2)
+(5,'kom','kom','Комірник Вася',1);
 
---
--- Table structure for table `category_commodity`
---
+INSERT INTO flows VALUES
+(1,2,3,10, "Завезені 10 бурів",'2014-05-28 13:11:0'),
+(2,12,4,-1, "Взята ізолента. Замовлення №111",'2014-05-30 11:11:0'),
+(3,13,3,25, "Завезено 25 кг цвяхів",'2014-06-01 17:54:0');
 
-DROP TABLE IF EXISTS `category_commodity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category_commodity` (
-  `CATEGORY_id` bigint(20) NOT NULL,
-  `commodityList_id` bigint(20) NOT NULL,
-  UNIQUE KEY `commodityList_id` (`commodityList_id`),
-  KEY `FK9770290CDAAB7D81` (`commodityList_id`),
-  CONSTRAINT `FK9770290CDAAB7D81` FOREIGN KEY (`commodityList_id`) REFERENCES `commodity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `category_commodity`
---
 
-LOCK TABLES `category_commodity` WRITE;
-/*!40000 ALTER TABLE `category_commodity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `category_commodity` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `commodity`
---
-
-DROP TABLE IF EXISTS `commodity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `commodity` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `measurementUnit` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `storage_id` bigint(20) DEFAULT NULL,
-  `MEASUREMENT` int(11) NOT NULL,
-  `category_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK4D84270D3F04DE12` (`storage_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `commodity`
---
-
-LOCK TABLES `commodity` WRITE;
-/*!40000 ALTER TABLE `commodity` DISABLE KEYS */;
-INSERT INTO `commodity` VALUES (2,'Бур Hitachi 40mm',0,'Бур Hitachi 40mm',NULL,0,NULL),(3,'Дюбель универсальный 6*40',0,'Дюбель 6*40',NULL,0,NULL),(4,'Манометричний коллектор',0,'Манометричний коллектор',NULL,0,NULL),(5,'Трубогиб пружинный 1/4 СТ-102-04',0,'Трубогиб пружинный 1/4',NULL,0,NULL),(6,'Трубогиб пружинный 3/8 СТ-102-06',0,'Трубогиб пружинный 3/8',NULL,0,NULL),(7,'Труборез CT 127 (3-16 мм)',0,'Труборез 3-16 мм',NULL,0,NULL),(8,'Вальцовка СТ-806АМ дюймовая',0,'Вальцовка дюймовая',NULL,0,NULL),(9,'Манометрический коллектор 536G R-22,12,407+3шланга (90см.) в комплекте Харьков',0,'Манометрический коллектор 536G ',NULL,0,NULL),(10,'хладагентом R22.Прикольний',2,'хладагентом R22',NULL,0,NULL),(11,'хладагентом R32.Круче',2,'хладагентом R32',NULL,0,NULL),(12,'Ізолента чорна',4,'Ізолента чорна',NULL,0,NULL),(13,'Цвяхи стальні 100мм',1,'Цвяхи і 100мм',NULL,0,NULL);
-/*!40000 ALTER TABLE `commodity` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `flowitems`
---
-
-DROP TABLE IF EXISTS `flowitems`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `flowitems` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `quantity` int(11) DEFAULT NULL,
-  `commodity_id` bigint(20) DEFAULT NULL,
-  `flow_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `FK4D3D83326C06C5F` (`commodity_id`),
-  KEY `FK4D3D833225A8D972` (`flow_id`),
-  CONSTRAINT `FK4D3D833225A8D972` FOREIGN KEY (`flow_id`) REFERENCES `flows` (`id`),
-  CONSTRAINT `FK4D3D83326C06C5F` FOREIGN KEY (`commodity_id`) REFERENCES `commodity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `flowitems`
---
-
-LOCK TABLES `flowitems` WRITE;
-/*!40000 ALTER TABLE `flowitems` DISABLE KEYS */;
-/*!40000 ALTER TABLE `flowitems` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `flows`
---
-
-DROP TABLE IF EXISTS `flows`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `flows` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `date` datetime DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `FK3FE2CE5B83427D5` (`user_id`),
-  CONSTRAINT `FK3FE2CE5B83427D5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `flows`
---
-
-LOCK TABLES `flows` WRITE;
-/*!40000 ALTER TABLE `flows` DISABLE KEYS */;
-/*!40000 ALTER TABLE `flows` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `flows_flowitems`
---
-
-DROP TABLE IF EXISTS `flows_flowitems`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `flows_flowitems` (
-  `FLOWS_id` bigint(20) NOT NULL,
-  `flowItems_id` bigint(20) NOT NULL,
-  UNIQUE KEY `flowItems_id` (`flowItems_id`),
-  KEY `FK12499058CC53923B` (`FLOWS_id`),
-  KEY `FK12499058B9953101` (`flowItems_id`),
-  CONSTRAINT `FK12499058B9953101` FOREIGN KEY (`flowItems_id`) REFERENCES `flowitems` (`id`),
-  CONSTRAINT `FK12499058CC53923B` FOREIGN KEY (`FLOWS_id`) REFERENCES `flows` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `flows_flowitems`
---
-
-LOCK TABLES `flows_flowitems` WRITE;
-/*!40000 ALTER TABLE `flows_flowitems` DISABLE KEYS */;
-/*!40000 ALTER TABLE `flows_flowitems` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orderitems`
---
-
-DROP TABLE IF EXISTS `orderitems`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `orderitems` (
-  `ORDERS_id` bigint(20) NOT NULL,
-  `store_id` bigint(20) NOT NULL,
-  UNIQUE KEY `store_id` (`store_id`),
-  KEY `FK4A8CAD329384F17B` (`ORDERS_id`),
-  KEY `FK4A8CAD3211613B92` (`store_id`),
-  CONSTRAINT `FK4A8CAD3211613B92` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`),
-  CONSTRAINT `FK4A8CAD329384F17B` FOREIGN KEY (`ORDERS_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orderitems`
---
-
-LOCK TABLES `orderitems` WRITE;
-/*!40000 ALTER TABLE `orderitems` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orderitems` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `orders` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `completed` tinyint(1) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK8B7256E5B83427D5` (`user_id`),
-  CONSTRAINT `FK8B7256E5B83427D5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `storage`
---
-
-DROP TABLE IF EXISTS `storage`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `storage` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `quantity` int(11) NOT NULL,
-  `commodity_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `FKBA7BCE5B6C06C5F` (`commodity_id`),
-  CONSTRAINT `FKBA7BCE5B6C06C5F` FOREIGN KEY (`commodity_id`) REFERENCES `commodity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `storage`
---
-
-LOCK TABLES `storage` WRITE;
-/*!40000 ALTER TABLE `storage` DISABLE KEYS */;
-/*!40000 ALTER TABLE `storage` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `storage_commodity`
---
-
-DROP TABLE IF EXISTS `storage_commodity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `storage_commodity` (
-  `STORAGE_STORAGE_ID` bigint(20) NOT NULL,
-  `commodity_COMMODITY_ID` bigint(20) NOT NULL,
-  UNIQUE KEY `commodity_COMMODITY_ID` (`commodity_COMMODITY_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `storage_commodity`
---
-
-LOCK TABLES `storage_commodity` WRITE;
-/*!40000 ALTER TABLE `storage_commodity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `storage_commodity` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `store`
---
-
-DROP TABLE IF EXISTS `store`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `store` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `quantity` int(11) DEFAULT NULL,
-  `commodity_COMMODITY_ID` bigint(20) DEFAULT NULL,
-  `order_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK4B900C1F4600872` (`order_id`),
-  CONSTRAINT `FK4B900C1F4600872` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `store`
---
-
-LOCK TABLES `store` WRITE;
-/*!40000 ALTER TABLE `store` DISABLE KEYS */;
-/*!40000 ALTER TABLE `store` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `login` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `role` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'order','OrderUser','order',2),(2,'mind','Sergiy Lesko','medusa',2),(3,'war','Worker','war',1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-02-17 22:27:56
